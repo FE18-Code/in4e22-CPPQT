ea5e5d9a21233dd85ed85b6957dae65f6bfa0f50
ams <ams.gh@hmamail.com>
2017-05-12 03:31:43 +0200

    class NbreCplx : base pattern filled + doc
    constructors (default + values + from object)
    priavte attr + getters & setters
    coord system scheme : enum type rect/polar + handle in constructors
    
    ** full Doxygen style code documentation (inside header) **

--- NbreCplx.cpp
+++ NbreCplx.cpp
@@ -1,1 +1,103 @@
 #include "NbreCplx.h"
+
+
+
+/* constructors & destructor */
+
+NbreCplx::NbreCplx() {
+    NbreCplx(0, 0, COORD_RECTANGULAR);
+}
+
+NbreCplx::NbreCplx(const NbreCplx & from) {
+    this->setCoord_representation(from.getCoord_representation());
+    NbreCplx::cpCoord(*this, from);
+
+}
+
+NbreCplx::NbreCplx(float_t val1, float_t val2, coord_system representation=D_COORDSYST_DEFAULT) {
+
+    /* first setting up representation */
+    this->setCoord_representation(representation);
+
+    /* then filling up values */
+    NbreCplx::cpCoord(*this, val1, val2, representation);
+}
+
+NbreCplx::~NbreCplx() {
+    /* void */
+}
+
+
+
+/* getters & setters */
+
+float_t NbreCplx::getCoord_rect_a() const {
+    return coord_rect_a;
+}
+
+void NbreCplx::setCoord_rect_a(float_t coord_rect_a) {
+    NbreCplx::coord_rect_a = coord_rect_a;
+}
+
+float_t NbreCplx::getCoord_rect_b() const {
+    return coord_rect_b;
+}
+
+void NbreCplx::setCoord_rect_b(float_t coord_rect_b) {
+    NbreCplx::coord_rect_b = coord_rect_b;
+}
+
+float_t NbreCplx::getCoord_polr_x() const {
+    return coord_polr_x;
+}
+
+void NbreCplx::setCoord_polr_x(float_t coord_polr_x) {
+    NbreCplx::coord_polr_x = coord_polr_x;
+}
+
+float_t NbreCplx::getCoord_polr_y() const {
+    return coord_polr_y;
+}
+
+void NbreCplx::setCoord_polr_y(float_t coord_polr_y) {
+    NbreCplx::coord_polr_y = coord_polr_y;
+}
+
+coord_system NbreCplx::getCoord_representation() const {
+    return coord_representation;
+}
+
+void NbreCplx::setCoord_representation(coord_system coord_representation) {
+    NbreCplx::coord_representation = coord_representation;
+}
+
+
+
+/* private functions */
+
+void NbreCplx::cpCoord(NbreCplx & dest, const NbreCplx & from){
+    float_t val1;
+    float_t val2;
+    coord_system repr = from.getCoord_representation();
+
+    if(repr == COORD_RECTANGULAR){
+        val1 = from.coord_rect_a;
+        val2 = from.coord_rect_b;
+    }else{ /* polar */
+        val1 = from.coord_polr_x;
+        val2 = from.coord_polr_y;
+    }
+    NbreCplx::cpCoord(dest, val1, val2, repr);
+}
+
+void NbreCplx::cpCoord(NbreCplx & dest, const float_t & val1, const float_t & val2, const coord_system & representation=D_COORDSYST_DEFAULT){
+    dest.setCoord_representation(representation);
+
+    if(dest.getCoord_representation() == COORD_RECTANGULAR){
+        dest.setCoord_rect_a(val1);
+        dest.setCoord_rect_b(val2);
+    }else{ /* polar */
+        dest.setCoord_polr_x(val1);
+        dest.setCoord_polr_y(val2);
+    }
+}
--- NbreCplx.h
+++ NbreCplx.h
@@ -1,11 +1,163 @@
+/** @file NbreCplx.h
+ *
+ * NbreCplx class header
+ */
+
 #ifndef TD_INTRO_NBRECPLX_H
 #define TD_INTRO_NBRECPLX_H
 
 
+#include <cmath>
+
+#define D_COORDSYST_DEFAULT COORD_RECTANGULAR /**< default representation system @see coord_system */
+
+/** @enum current representation system
+ *
+ */
+enum coord_system {
+    COORD_RECTANGULAR, /**< using rectangular system a+ib */
+    COORD_POLAR /**< using polar system r*e^(i*theta) */
+};
+
+/** NbreCplx class
+ *
+ */
 class NbreCplx {
 
 
-};
+public:
+    /* ========================= */
+    /* constructors & destructor */
+    /* ========================= */
 
+    /** default void constructor
+     * creates NbreCplx plus
+     * - sets rect representation
+     * - sets members to 0
+     */
+    NbreCplx::NbreCplx();
+
+    /** from another NbreCplx object constructor
+     *
+     * @param from source objects to read values (read only)
+     */
+    NbreCplx::NbreCplx(const NbreCplx & from);
+
+    /** full setup constructor
+     *
+     * @param val1 a member in rect representation, module in polar
+     * @param val2 b member in rect representation, angle in polar
+     * @param representation system to be used for complex representation : @see coord_system defined in class header
+     */
+    NbreCplx::NbreCplx(float_t val1, float_t val2, coord_system representation=D_COORDSYST_DEFAULT);
+
+    /** standard destructor
+     *
+     */
+    NbreCplx::~NbreCplx();
+
+
+    /* ================= */
+    /* getters & setters */
+    /* ================= */
+
+    /** getter rect a
+     *
+     * @return a member (rectangular)
+     */
+    float_t getCoord_rect_a() const;
+
+    /** setter rect a
+     *
+     * @param a member (rectangular)
+     */
+    void setCoord_rect_a(float_t coord_rect_a);
+
+    /** getter rect b
+     *
+     * @return b member (rectangular)
+     */
+    float_t getCoord_rect_b() const;
+
+    /** setter rect b
+     *
+     * @param b member (rectangular)
+     */
+    void setCoord_rect_b(float_t coord_rect_b);
+
+    /** getter pol x
+     *
+     * @return r member (polar)
+     */
+    float_t getCoord_polr_x() const;
+
+    /** setter pol x
+     *
+     * @param r member (polar)
+     */
+    void setCoord_polr_x(float_t coord_polr_x);
+
+    /** getter pol y
+     *
+     * @return theta member (polar)
+     */
+    float_t getCoord_polr_y() const;
+
+    /** setter pol y
+     *
+     * @param theta member (polar)
+     */
+    void setCoord_polr_y(float_t coord_polr_y);
+
+    /** getter coord system
+     *
+     * @return current complex representation system
+     */
+    coord_system getCoord_representation() const;
+
+    /** setter coord system
+     *
+     * @param coord_representation new system to use for complex representation
+     */
+    void setCoord_representation(coord_system coord_representation);
+
+
+private:
+    /* ================= */
+    /* private functions */
+    /* ================= */
+
+    /** copy NbreCplx attributes : objects from->dest
+     *
+     * @param dest destination object for values
+     * @param from source object (read only)
+     */
+    static void cpCoord(NbreCplx & dest, const NbreCplx & from);
+
+    /** copy values to NbreCplx attributes : floats -> object dest
+     *
+     * @param dest destination object for values
+     * @param val1 either rect_a or pol_module (selected by representation)
+     * @param val2 either rect_b or pol_angle (selected by representation)
+     * @param representation either rect or pol : @see coord_system
+     */
+    static void cpCoord(NbreCplx & dest, const float_t & val1, const float_t & val2, const coord_system & representation=D_COORDSYST_DEFAULT);
+
+
+    /* ================== */
+    /* private attributes */
+    /* ================== */
+
+    float_t coord_rect_a; /**< real member in rect representation a+ib */
+
+    float_t coord_rect_b; /**< complex member in rect representation a+ib */
+
+    float_t coord_polr_x; /**< module in polar representation */
+
+    float_t coord_polr_y; /**< angle in polar representation */
+
+    coord_system coord_representation; /**< rect/polar system */
+
+};
 
 #endif //TD_INTRO_NBRECPLX_H
